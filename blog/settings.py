00e2760f81d1a022c1bc58562d2521470a5d3d1d
kamon <kamon@kamons-MacBook-Pro.local>
2020-09-20 22:23:01 +1200

    new  qurirement file

--- blog/settings.py
+++ blog/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+
+https://www.youtube.com/watch?v=66IeCXomWqk&t=41s&ab_channel=Developer%27sHub
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
